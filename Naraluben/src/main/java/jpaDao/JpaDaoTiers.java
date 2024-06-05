0a2492b485722167b278f99647327d3fd2a74567
Nawel18 <nawel.merabetathlet14@gmail.com>
2024-06-05 21:03:01 +0200

    fonction connexion

--- Naraluben/src/main/java/jpaDao/JpaDaoTiers.java
+++ Naraluben/src/main/java/jpaDao/JpaDaoTiers.java
@@ -1,6 +1,7 @@
 package jpaDao;
 
 import dao.DaoTiers;
+import jakarta.persistence.NoResultException;
 import metier.Tiers;
 
 import java.util.List;
@@ -13,6 +14,21 @@ public class JpaDaoTiers extends JpaDao<Tiers> implements DaoTiers {
 
     public List<Tiers> findAll() {
         return super.findAll(Tiers.class);
+    }
+
+    public Tiers findByEmailAndPassword(String email, String password) {
+        Tiers tiers = null;
+
+        try {
+            tiers = (Tiers) entityManager.createQuery("SELECT o FROM Tiers o WHERE o.email = :email AND o.password = :password", Tiers.class)
+                    .setParameter("email", email)
+                    .setParameter("password", password)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            // Handle no result case
+        }
+
+        return tiers;
     }
 
 }
